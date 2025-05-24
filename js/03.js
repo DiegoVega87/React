e6d555936663807bfcf7a151114ab97dba6293fa
Diego Vega <diegovega@me.com>
2025-05-24 18:47:32 +0200

    Data types

---
+++ js/03.js
@@ -0,0 +1,46 @@
+// Tipos de Dato
+
+// Unfined
+// let cliente;
+// console.log(cliente);
+// console.log(typeof cliente);
+
+// Strings
+// const student = "Juan";
+// console.log(student);
+// console.log(typeof student);
+
+/* const product = "Monitor de 49 pulgadas";
+console.log(product);
+console.log(typeof product); */
+
+// Numbers
+// const num1 = 20.2;
+// const num2 = 30;
+// const num3 = -100;
+// const num4 = "20";
+
+// console.log(typeof num1)
+// console.log(typeof num2)
+// console.log(typeof num3)
+// console.log(typeof num4)
+
+// BigInt
+// const bigNumber = BigInt(1932465468498494989849984948)
+// console.log(typeof bigNumber)
+
+// Boolean
+// const discount = false;
+// console.log(typeof discount);
+
+// Null
+// const discount = null;
+// console.log(discount);
+
+// let price = undefined;
+// console.log(price);
+
+// Symbol
+// const firstSymbol = Symbol(30);
+// const secondSymbol = Symbol(30);
+// console.log(firstSymbol === secondSymbol);
