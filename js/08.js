97f6083c43636b7a209c601c9d277e6695379f00
DiegoVega87 <diegovega@me.com>
2025-05-27 08:43:31 +0200

    Template Strings and Concatenations

---
+++ js/08.js
@@ -0,0 +1,13 @@
+// Template String and concatenation
+
+const product = "Tablet 12 inches";
+const price = 400;
+const brand = "TechBrand";
+
+console.log("The produt is: " + product);
+// Template String
+console.log(`The product is: ${product}`);
+
+console.log(product + " $" + price + " Dollars, brand: " + brand);
+// Template String
+console.log(`${product} $${price} Dollars, brand: ${brand}`);
