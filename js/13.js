49228e2774c0c21b7c8a8fc4b61e592674a9468d
DiegoVega87 <diegovega@me.com>
2025-05-30 12:21:10 +0200

    functions

---
+++ js/13.js
@@ -0,0 +1,9 @@
+// Function Declaration
+//Default parameters -> when a parameter is missing, the default parameter substitudes it.
+function sum(num1 = 0, num2 = 0) {
+  console.log(num1);
+  console.log(num2);
+  console.log(num1 + num2);
+}
+
+sum(6);
