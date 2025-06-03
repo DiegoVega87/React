5bd24976fa663504dd30b8dedbfbfcee73d12132
DiegoVega87 <diegovega@me.com>
2025-06-03 14:35:54 +0200

    Conditionals

---
+++ js/18.js
@@ -0,0 +1,24 @@
+// Condicionales
+
+const available = 4000;
+const withdraw = 200;
+
+const auth = true;
+
+if (available > withdraw) {
+  // Meets the condition
+  console.log("Can withdraw");
+} else {
+  // Condition not met
+  console.log("Not enough funds");
+}
+
+/**
+ * > greater than
+ * < less than
+ * >= greater or egual
+ * <= less than or equal
+ * == equal
+ * === Strictly equal
+ * !== Not equal
+ */
