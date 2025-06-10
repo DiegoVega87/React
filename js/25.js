a1f101d2a20a6982283b1aaf0643ef2c021ec65e
DiegoVega87 <diegovega@me.com>
2025-06-10 13:46:09 +0200

    Export/import default modules and arrow functions

---
+++ js/25.js
@@ -0,0 +1,9 @@
+import multiply, { sum, substract, division } from "./functions.js";
+
+const result = substract(20, 10);
+const result1 = multiply(20, 10);
+const result2 = division(20, 10);
+
+console.log(result);
+console.log(result1);
+console.log(result2);
--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,15 @@
-function sum(n1, n2) {
+export function sum(n1, n2) {
   return n1 + n2;
 }
 
-function substract(n1, n2) {
-  return n1 - n2;
+// Arrow functions
+export const substract = (n1, n2) => n1 - n2;
+
+// Only one export default per file!
+export default function multiply(n1, n2) {
+  return n1 * n2;
 }
 
-export { sum, substract };
+export function division(n1, n2) {
+  return n1 / n2;
+}
